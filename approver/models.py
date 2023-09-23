9b83f2337d400a322e965e24c6550517a6af73e6
thiru-lifo <122973298+thiru-lifo@users.noreply.github.com>
2023-09-23 18:59:18 +0530

    config-table

--- approver/models.py
+++ approver/models.py
@@ -7,11 +7,14 @@ class Config(models.Model):
 	name = models.CharField(max_length=150, blank=True, null=True)
 	code = models.CharField(max_length=150, blank=True, null=True)
 	desc = models.TextField(blank=True, null=True)
-	created = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-	modified = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="config_modified_id")
-	created_ip = models.GenericIPAddressField(null=True)
-	modified_ip = models.GenericIPAddressField(blank=True, null=True)
 	status = models.SmallIntegerField(choices=((1,'Active'),(2,'Inactive'),(3,'Delete')))
+	created_on = models.DateTimeField(auto_now_add=True)
+	created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+	created_ip = models.GenericIPAddressField(null=True)
+	modified_on = models.DateTimeField(auto_now=True, blank=True, null=True)
+	modified_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="config_modified_id")
+	modified_ip = models.GenericIPAddressField(blank=True, null=True)
+
 	def _str_(self):
 	    return self.name
 
--- approver/views.py
+++ approver/views.py
@@ -1,10 +1,87 @@
+# from django.shortcuts import render
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from NavyTrials import language,error,settings,common
+# from access.views import Common
+# from . import models
+
+
+from django.views.decorators.clickjacking import xframe_options_exempt
+from urllib import response
 from django.shortcuts import render
-from rest_framework.views import APIView
+from functools import partial
+from queue import Empty
+from unicodedata import name
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+#from rest_framework import authentication, AllowAny
+#from rest_framework.permissions import Is_Authenticated
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from django.http import Http404
+from functools import reduce
+import operator
+from django.db.models import Q
+from collections import defaultdict
+from rest_framework import filters
+from configuration.models import Templatestable
+
+
+from NavyTrials import language,error,settings,common
+from access.views import Common
+
+from collections import namedtuple
+
+from django.template import Template, Context
+from datetime import datetime
+import time
+from django.template import loader
+from django.http import HttpResponse
+from io import BytesIO
+from xhtml2pdf import pisa
+
+from django.template import Context, Template
+import requests
+import json
+import barcode
+from barcode.writer import ImageWriter
+from django.utils.crypto import get_random_string
 from . import models
+#from . import serializer
+#from . import serializer as cSerializer
+from configuration.models import Approval
+from notification.models import NotificationUser,NotificationUserLog
+#from master import models as masterModels
+from master import serializer as masterSerializer
+from access import models as accessModels
+#from accounts import models as accountsModels
+
+from notification import models as notificationModels
+from access import models as accessSerializer
+#import pandas as pd
+import uuid
+import os
+import csv
+from django.core.files.storage import FileSystemStorage
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+from django.core.mail import send_mail
+from django.core.mail import EmailMessage
+
+from django.http import JsonResponse
+from django.template.loader import render_to_string
 
 
 class ConfigCRUD(APIView):
+    authentication_classes = [] #disables authentication
+    permission_classes = [] #disables permission
 
     def post(self,request, pk = None):
 
@@ -28,13 +105,14 @@ class ConfigCRUD(APIView):
             )
         else:
 
-            if request.data["id"]==None:
+            #print(request.data,"adad")
+            if request.data["id"]=="":
 
                 models.Config.objects.create(
                     name = request.data["name"],
                     code = request.data["code"],
                     desc = request.data["desc"],
-                    created_id = request.user.id,
+                    created_by_id = request.user.id,
                     created_ip = Common.get_client_ip(request),
                     status = request.data["status"]
                 )
@@ -44,7 +122,7 @@ class ConfigCRUD(APIView):
                     name = request.data["name"],
                     code = request.data["code"],
                     desc = request.data["desc"],
-                    modified_id = request.user.id,
+                    modified_by_id = request.user.id,
                     modified_ip = Common.get_client_ip(request),
                     status = request.data["status"]
                 )
