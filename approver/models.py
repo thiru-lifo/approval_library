1070cf037043e131f5da8b98dae925d39c0b2704
thiru-lifo <122973298+thiru-lifo@users.noreply.github.com>
2023-09-25 11:42:36 +0530

    approved_config

--- approver/models.py
+++ approver/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from accounts.models import User
+from access.models import AccessUserRoles, ProcessFlow
 # Create your models here.
 
 
@@ -22,3 +23,27 @@ class Config(models.Model):
 	    db_table = "master.config"
 	    verbose_name = "Config"
 	    verbose_name_plural = "Config"
+
+
+class ApprovedConfig(models.Model):
+
+	config = models.ForeignKey(Config, on_delete= models.CASCADE, null=True)
+	role = models.ForeignKey(AccessUserRoles, on_delete=models.CASCADE, null=True)
+	user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+	type = models.SmallIntegerField(choices=((1,'Recommender'),(2,'Approver')))
+	level = models.SmallIntegerField(null=True, blank=True)
+	status = models.SmallIntegerField(choices=((1,'Active'),(2,'Inactive'),(3,'Delete')))
+	created_on = models.DateTimeField(auto_now_add=True)
+	created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="approver_config_created_by")
+	created_ip = models.GenericIPAddressField(null=True)
+	modified_on = models.DateTimeField(auto_now=True, blank=True, null=True)
+	modified_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="approver_config_modified_by")
+	modified_ip = models.GenericIPAddressField(blank=True, null=True)
+
+	def _str_(self):
+	    return self.config
+
+	class Meta:
+	    db_table = "master.approved_config"
+	    verbose_name = "ApprovedConfig"
+	    verbose_name_plural = "ApprovedConfig"
--- approver/views.py
+++ approver/views.py
@@ -160,3 +160,95 @@ class ConfigList(APIView):
             {"status": error.context["success_code"], "data": config},
             status=status.HTTP_200_OK,
         )
+
+
+class ApprovedConfigCRUD(APIView):
+    authentication_classes = [] #disables authentication
+    permission_classes = [] #disables permission
+
+    def post(self,request, pk = None):
+
+        if "config_id" not in request.data and request.data["status"] != 3:
+            return Response(
+                {
+                    "status": error.context["error_code"],
+                    "message": "Config Id"
+                    + language.context[language.defaultLang]["missing"],
+                },
+                status=status.HTTP_200_OK,
+            )
+        elif "role_id" not in request.data and request.data["status"] != 3:
+            return Response(
+                {
+                    "status": error.context["error_code"],
+                    "message": "Role Id"
+                    + language.context[language.defaultLang]["missing"],
+                },
+                status=status.HTTP_200_OK,
+            )
+        if "user_id" not in request.data and request.data["status"] != 3:
+            return Response(
+                {
+                    "status": error.context["error_code"],
+                    "message": "User Id"
+                    + language.context[language.defaultLang]["missing"],
+                },
+                status=status.HTTP_200_OK,
+            )
+        elif "type" not in request.data and request.data["status"] != 3:
+            return Response(
+                {
+                    "status": error.context["error_code"],
+                    "message": "Type"
+                    + language.context[language.defaultLang]["missing"],
+                },
+                status=status.HTTP_200_OK,
+            )
+        elif "level" not in request.data and request.data["status"] != 3:
+            return Response(
+                {
+                    "status": error.context["error_code"],
+                    "message": "Level"
+                    + language.context[language.defaultLang]["missing"],
+                },
+                status=status.HTTP_200_OK,
+            )
+        else:
+
+            #print(request.data,"adad")
+            if request.data["id"]==None:
+                #print(request.data['name'],"adad2222")
+                models.Config.objects.create(
+                    config_id = request.data["config_id"],
+                    role_id = request.data["role_id"],
+                    user_id = request.data["user_id"],
+                    type = request.data["type"],
+                    level = request.data["level"],
+                    created_by_id = request.user.id,
+                    created_ip = Common.get_client_ip(request),
+                    status = request.data["status"]
+                )
+                return Response({"status" :error.context['success_code'], "message":'Approved config created successfully'}, status=status.HTTP_200_OK)
+            else:
+                if request.data["status"] != 3:
+
+                    models.Config.objects.filter(id=request.data["id"]).update(
+                        config_id = request.data["config_id"],
+                        role_id = request.data["role_id"],
+                        user_id = request.data["user_id"],
+                        type = request.data["type"],
+                        level = request.data["level"],
+                        modified_by_id = request.user.id,
+                        modified_ip = Common.get_client_ip(request),
+                        status = request.data["status"]
+                    )
+
+                    return Response({"status" :error.context['success_code'], "message":'Approved config updated successfully'}, status=status.HTTP_200_OK)
+
+                else:
+
+                    models.Config.objects.filter(id=request.data["id"]).update(
+                        status = request.data["status"]
+                    )
+
+                    return Response({"status" :error.context['success_code'], "message":'Approved config deleted successfully'}, status=status.HTTP_200_OK)
